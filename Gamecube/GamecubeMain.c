6f65aab8b2f026a87788d7a58f0c6181863dc142
emukidid <emukidid@0c9105cc-704f-0410-9c51-a50a6f8d0bd5>
2008-06-12 12:06:16 +0000

    Added makefile and gamecubemain.c, of course there's alot of issues compiling it still

---
+++ Gamecube/GamecubeMain.c
@@ -0,0 +1,162 @@
+/*  Pcsx - Pc Psx Emulator
+ *  Copyright (C) 1999-2002  Pcsx Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <gccore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+#include "PsxCommon.h"
+
+/* function prototypes */
+int SysInit();
+void SysReset();
+void SysClose();
+void SysPrintf(char *fmt, ...);
+void *SysLoadLibrary(char *lib);
+void *SysLoadSym(void *lib, char *sym);
+char *SysLibError();
+void SysCloseLibrary(void *lib);
+void SysUpdate();
+void SysRunGui();
+void SysMessage(char *fmt, ...);
+
+
+/* draw background */
+void draw_splash(void)
+{
+   printf("Splash Screen");
+}
+
+long LoadCdBios;
+
+int main(int argc, char *argv[]) {
+/*	char *file = NULL;
+	int runcd = 0;
+	int loadst = 0;
+	int i;
+*/
+    draw_splash();
+	
+	/* Configure pcsx */
+	memset(&Config, 0, sizeof(PcsxConfig));
+	strcpy(Config.Bios, "scph1001.bin"); // Use actual BIOS
+	strcpy(Config.BiosDir, "/PCSX/");
+//	Config.Cpu = 1;	//?
+	Config.Cpu = 0;
+//	Config.CdTiming = 0;	//no longer used
+	Config.PsxOut = 1;
+//	Config.HLE = 1;	//?
+	
+    SysPrintf("start main()\r\n");
+
+	if (SysInit() == -1) return 1;
+
+	/* Start gui */
+//	menu_start();
+
+	OpenPlugins();
+	SysReset();
+
+    SysPrintf("CheckCdrom()\r\n");
+	CheckCdrom();
+	
+    SysPrintf("Load()\r\n");
+//	Load("/rd/pdx-dlcm.psx");
+	LoadCdrom();
+	
+    SysPrintf("Execute()\r\n");
+	psxCpu->Execute();
+
+	return 0;
+}
+
+int SysInit() {
+    SysPrintf("start SysInit()\r\n");
+
+    SysPrintf("psxInit()\r\n");
+	psxInit();
+
+    SysPrintf("LoadPlugins()\r\n");
+	LoadPlugins();
+    SysPrintf("LoadMcds()\r\n");
+//	LoadMcds(Config.Mcd1, Config.Mcd2);
+
+	SysPrintf("end SysInit()\r\n");
+	return 0;
+}
+
+void SysReset() {
+    SysPrintf("start SysReset()\r\n");
+	psxReset();
+	SysPrintf("end SysReset()\r\n");
+}
+
+void SysClose() {
+	psxShutdown();
+	ReleasePlugins();
+
+	if (emuLog != NULL) fclose(emuLog);
+}
+
+void SysPrintf(char *fmt, ...) {
+	va_list list;
+	char msg[512];
+
+	va_start(list, fmt);
+	vsprintf(msg, fmt, list);
+	va_end(list);
+
+	if (Config.PsxOut) printf ("%s", msg);
+#if defined (CPU_LOG) || defined(DMA_LOG) || defined(CDR_LOG) || defined(HW_LOG) || \
+	defined(BIOS_LOG) || defined(GTE_LOG) || defined(PAD_LOG)
+	fprintf(emuLog, "%s", msg);
+#endif
+}
+
+void *SysLoadLibrary(char *lib) {
+//	return dlopen(lib, RTLD_NOW);
+}
+
+void *SysLoadSym(void *lib, char *sym) {
+//	return dlsym(lib, sym);
+}
+
+char *SysLibError() {
+//	return dlerror();
+}
+
+void SysCloseLibrary(void *lib) {
+//	dlclose(lib);
+}
+
+void SysUpdate() {
+//	PADhandleKey(PAD1_keypressed());
+//	PADhandleKey(PAD2_keypressed());
+}
+
+void SysRunGui() {
+//	RunGui();
+}
+
+void SysMessage(char *fmt, ...) {
+	
+}
