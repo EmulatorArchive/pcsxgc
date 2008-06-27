baf1ef27c90b6623387934d7276b18a44aa95cce
emukidid <emukidid@0c9105cc-704f-0410-9c51-a50a6f8d0bd5>
2008-06-27 09:19:29 +0000

    Cleaned up PAD Plugin, now full fledged analog pads :)

--- Gamecube/PlugPAD.c
+++ Gamecube/PlugPAD.c
@@ -1,7 +1,8 @@
-/* 	super basic PAD plugin for PCSX Gamecube
-	by emu_kidid based on the DC port
+/* 	
+	Basic Analog PAD plugin for PCSX Gamecube
+	by emu_kidid based on the DC/MacOSX HID plugin
 	
-	TODO: Re-write for proper PADs, Trigger/Analog support
+	TODO: Rumble?
 */
 
 #include <gccore.h>
@@ -17,6 +18,23 @@
 #include <ogc/pad.h>
 #include "plugins.h"
 #include "PsxCommon.h"
+#include "PSEmu_Plugin_Defs.h"
+
+/* Button Bits */
+#define PSX_BUTTON_TRIANGLE ~(1 << 12)
+#define PSX_BUTTON_SQUARE 	~(1 << 15)
+#define PSX_BUTTON_CROSS	~(1 << 14)
+#define PSX_BUTTON_CIRCLE	~(1 << 13)
+#define PSX_BUTTON_L2		~(1 << 8)
+#define PSX_BUTTON_R2		~(1 << 9)
+#define PSX_BUTTON_L1		~(1 << 10)
+#define PSX_BUTTON_R1		~(1 << 11)
+#define PSX_BUTTON_SELECT	~(1 << 0)
+#define PSX_BUTTON_START	~(1 << 3)
+#define PSX_BUTTON_DUP		~(1 << 4)
+#define PSX_BUTTON_DRIGHT	~(1 << 5)
+#define PSX_BUTTON_DDOWN	~(1 << 6)
+#define PSX_BUTTON_DLEFT	~(1 << 7)
 
 long  PadFlags = 0;
 
@@ -30,11 +48,11 @@ long PAD__init(long flags) {
 
 	SysPrintf("end PAD_init()\r\n");
 	
-	return 0;
+	return PSE_PAD_ERR_SUCCESS;
 }
 
 long PAD__shutdown(void) {
-	return 0;
+	return PSE_PAD_ERR_SUCCESS;
 }
 
 long PAD__open(void)
@@ -42,70 +60,103 @@ long PAD__open(void)
 //	SysPrintf("start PAD_open()\r\n");
 
 //	SysPrintf("end PAD_open()\r\n");
-	return 0;
+	return PSE_PAD_ERR_SUCCESS;
 }
 
 long PAD__close(void) {
-	return 0;
+	return PSE_PAD_ERR_SUCCESS;
 }
 
 long PAD__readPort1(PadDataS* pad) {
-//	SysPrintf("start PAD1_readPort()\r\n");
 	int b = PAD_ButtonsHeld(0);
 	uint16_t pad_status = 0xFFFF;	//bit pointless why is this done this way?
-	 
-	if (!(b & PAD_BUTTON_START))
-		pad_status &= ~(1<<3);
+	
+	/* PAD Buttons, Start = Start, Select = Start+Z, Cross = A, Square = B, Triangle = Y, Circle = X */
+	if (!((b & PAD_BUTTON_START) && (!(b & PAD_TRIGGER_Z))))
+		pad_status &= PSX_BUTTON_START;
+	if (!((b & PAD_BUTTON_START) && (b & PAD_TRIGGER_Z)))
+		pad_status &= PSX_BUTTON_SELECT;
 	if (!(b & PAD_BUTTON_A))
-		pad_status &= ~(1<<14);
+		pad_status &= PSX_BUTTON_CROSS;
 	if (!(b & PAD_BUTTON_B))
-		pad_status &= ~(1<<13);
+		pad_status &= PSX_BUTTON_SQUARE;
 	if (!(b & PAD_BUTTON_X))
-		pad_status &= ~(1<<15);
+		pad_status &= PSX_BUTTON_CIRCLE;
 	if (!(b & PAD_BUTTON_Y))
-		pad_status &= ~(1<<12);
+		pad_status &= PSX_BUTTON_TRIANGLE;
 	if (!(b & PAD_BUTTON_UP))
-		pad_status &= ~(1<<4);
+		pad_status &= PSX_BUTTON_DUP;
 	if (!(b & PAD_BUTTON_DOWN))
-		pad_status &= ~(1<<6);
+		pad_status &= PSX_BUTTON_DDOWN;
 	if (!(b & PAD_BUTTON_LEFT))
-		pad_status &= ~(1<<7);
+		pad_status &= PSX_BUTTON_DLEFT;
 	if (!(b & PAD_BUTTON_RIGHT))
-		pad_status &= ~(1<<5);
-			
-	pad->buttonStatus = pad_status;
-	pad->controllerType = 4; // standard
-//	printf("Pad1 Status: %04x\n", pad_status);
-//	SysPrintf("end PAD1_readPort()\r\n");
-	return 0;
+		pad_status &= PSX_BUTTON_DRIGHT;
+	/* Shoulder Buttons, L1 = L, R1 = R, L2 = L+Z, R2 = R+Z */
+	if (!((b & PAD_TRIGGER_Z) && (b & PAD_TRIGGER_R)))
+		pad_status &= PSX_BUTTON_R2;
+	if (!((b & PAD_TRIGGER_Z) && (b & PAD_TRIGGER_L)))
+		pad_status &= PSX_BUTTON_L2;
+	if (!((!(b & PAD_TRIGGER_Z)) && (b & PAD_TRIGGER_R)))
+		pad_status &= PSX_BUTTON_R1;
+	if (!((!(b & PAD_TRIGGER_Z)) && (b & PAD_TRIGGER_L)))
+		pad_status &= PSX_BUTTON_L1;
+					
+	
+	//adjust values by 128 cause psx values in range 0-255 where 128 is center position
+	pad->leftJoyX = PAD_StickX(0)+128;				//analog stick
+	pad->leftJoyY = PAD_StickY(0)+128;		
+	pad->rightJoyX = PAD_SubStickX(0)+128;			//C-stick (Left JoyStick)
+	pad->rightJoyY = PAD_SubStickY(0)+128;	
+	
+	pad->controllerType = PSE_PAD_TYPE_ANALOGPAD; 	// Analog Pad  (Right JoyStick)
+	pad->buttonStatus = pad_status;					//Copy Buttons
+	return PSE_PAD_ERR_SUCCESS;
 }
 
 long PAD__readPort2(PadDataS* pad) {
 	int b = PAD_ButtonsHeld(1);
 	uint16_t pad_status = 0xFFFF;	//bit pointless why is this done this way?
-	 
-	if (!(b & PAD_BUTTON_START))
-		pad_status &= ~(1<<3);
+	
+	/* PAD Buttons, Start = Start, Select = Start+Z, Cross = A, Square = B, Triangle = Y, Circle = X */
+	if (!((b & PAD_BUTTON_START) && (!(b & PAD_TRIGGER_Z))))
+		pad_status &= PSX_BUTTON_START;
+	if (!((b & PAD_BUTTON_START) && (b & PAD_TRIGGER_Z)))
+		pad_status &= PSX_BUTTON_SELECT;
 	if (!(b & PAD_BUTTON_A))
-		pad_status &= ~(1<<14);
+		pad_status &= PSX_BUTTON_CROSS;
 	if (!(b & PAD_BUTTON_B))
-		pad_status &= ~(1<<13);
+		pad_status &= PSX_BUTTON_SQUARE;
 	if (!(b & PAD_BUTTON_X))
-		pad_status &= ~(1<<15);
+		pad_status &= PSX_BUTTON_CIRCLE;
 	if (!(b & PAD_BUTTON_Y))
-		pad_status &= ~(1<<12);
+		pad_status &= PSX_BUTTON_TRIANGLE;
 	if (!(b & PAD_BUTTON_UP))
-		pad_status &= ~(1<<4);
+		pad_status &= PSX_BUTTON_DUP;
 	if (!(b & PAD_BUTTON_DOWN))
-		pad_status &= ~(1<<6);
+		pad_status &= PSX_BUTTON_DDOWN;
 	if (!(b & PAD_BUTTON_LEFT))
-		pad_status &= ~(1<<7);
+		pad_status &= PSX_BUTTON_DLEFT;
 	if (!(b & PAD_BUTTON_RIGHT))
-		pad_status &= ~(1<<5);
-			
-	pad->buttonStatus = pad_status;
-	pad->controllerType = 4; // standard
-//	printf("Pad1 Status: %04x\n", pad_status);
-//	SysPrintf("end PAD1_readPort()\r\n");
-	return 0;
+		pad_status &= PSX_BUTTON_DRIGHT;
+	/* Shoulder Buttons, L1 = L, R1 = R, L2 = L+Z, R2 = R+Z */
+	if (!((b & PAD_TRIGGER_Z) && (b & PAD_TRIGGER_R)))
+		pad_status &= PSX_BUTTON_R2;
+	if (!((b & PAD_TRIGGER_Z) && (b & PAD_TRIGGER_L)))
+		pad_status &= PSX_BUTTON_L2;
+	if (!((!(b & PAD_TRIGGER_Z)) && (b & PAD_TRIGGER_R)))
+		pad_status &= PSX_BUTTON_R1;
+	if (!((!(b & PAD_TRIGGER_Z)) && (b & PAD_TRIGGER_L)))
+		pad_status &= PSX_BUTTON_L1;
+					
+	
+	//adjust values by 128 cause psx values in range 0-255 where 128 is center position
+	pad->leftJoyX = PAD_StickX(1)+128;				//analog stick
+	pad->leftJoyY = PAD_StickY(1)+128;		
+	pad->rightJoyX = PAD_SubStickX(1)+128;			//C-stick (Left JoyStick)
+	pad->rightJoyY = PAD_SubStickY(1)+128;	
+	
+	pad->controllerType = PSE_PAD_TYPE_ANALOGPAD; 	// Analog Pad  (Right JoyStick)
+	pad->buttonStatus = pad_status;					//Copy Buttons
+	return PSE_PAD_ERR_SUCCESS;
 }
